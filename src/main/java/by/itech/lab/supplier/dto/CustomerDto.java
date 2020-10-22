dccec0cc2eebe1edb1461bac0ac5140713147766
IrynaZazyba <38259449+IrynaZazyba@users.noreply.github.com>
2020-10-22 15:00:07 +0300

    merge conflict

--- src/main/java/by/itech/lab/supplier/dto/CustomerDto.java
+++ src/main/java/by/itech/lab/supplier/dto/CustomerDto.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 
 @Data
 @Builder
-public class CustomerDto implements DefaultDto {
+public class CustomerDto implements BaseDto {
 
     private Long id;
     private String name;
