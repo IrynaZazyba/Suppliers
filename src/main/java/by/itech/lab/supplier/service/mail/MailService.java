abec9ebf4eb823931b6e542d8527f6c8abd54a5b
alesiahlushakova <42976656+alesiahlushakova@users.noreply.github.com>
2020-10-28 20:29:14 +0300

    mail service

---
+++ src/main/java/by/itech/lab/supplier/service/mail/MailService.java
@@ -0,0 +1,11 @@
+package by.itech.lab.supplier.service.mail;
+
+import by.itech.lab.supplier.domain.User;
+import by.itech.lab.supplier.dto.UserDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface MailService {
+    void sendMail(UserDto user, String url);
+
+}
---
+++ src/main/java/by/itech/lab/supplier/service/mail/MailServiceImpl.java
@@ -0,0 +1,38 @@
+package by.itech.lab.supplier.service.mail;
+
+
+import by.itech.lab.supplier.domain.User;
+import by.itech.lab.supplier.dto.UserDto;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.stringtemplate.v4.ST;
+import org.thymeleaf.TemplateEngine;
+
+@Service
+@AllArgsConstructor
+public class MailServiceImpl implements MailService {
+    private static final String LASTNAME = "username";
+    private static final String LINK = "link";
+    private static final String USERNAME = "adminMail";
+    private static final String PASSWORD = "password";
+    private static final String ACTIVATION_LINK = "Activation link";
+
+    private final TemplateEngine templateEngine;
+
+    public void sendMail(UserDto user, String url) {
+            Sender sender = new Sender(USERNAME, PASSWORD);
+            Email email = new Email();
+            email.setTo(user.getEmail());
+            email.setSubject(ACTIVATION_LINK);
+            email.setText(buildMessage(user, url));
+            sender.send(email);
+    }
+
+    private String buildMessage(UserDto user, String url) {
+        ST st = new ST("Hello, <username>! To activate Your profile, please follow the link below <link>");
+        st.add(LASTNAME, user.getUsername());
+        st.add(LINK, url);
+        return st.render();
+    }
+
+}
