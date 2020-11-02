0baa6f7ac602fa9bd191b3475f43fb8f79e941d0
IrynaZazyba <38259449+IrynaZazyba@users.noreply.github.com>
2020-11-02 12:50:41 +0300

    fixed security

--- src/main/java/by/itech/lab/supplier/auth/config/WebSecurityConfig.java
+++ src/main/java/by/itech/lab/supplier/auth/config/WebSecurityConfig.java
@@ -39,9 +39,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors()
-
-                .and()
+        http
                 .csrf().disable()
                 .logout()
                 .logoutUrl(URL_LOGOUT)
