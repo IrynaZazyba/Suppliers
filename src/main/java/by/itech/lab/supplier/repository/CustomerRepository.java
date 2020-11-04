60f0b61eba4fbb75144c93dfcb6e792ff081c132
IrynaZazyba <38259449+IrynaZazyba@users.noreply.github.com>
2020-11-04 18:24:15 +0300

    fixed Customer soft delete

--- src/main/java/by/itech/lab/supplier/repository/CustomerRepository.java
+++ src/main/java/by/itech/lab/supplier/repository/CustomerRepository.java
@@ -8,16 +8,14 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
-
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     @Query("select c from Customer c where :active is null or c.active=:active")
     Page<Customer> findByStatus(Pageable pageable, @Param("active") Boolean active);
 
     @Modifying
-    @Query("update Customer set deletedAt = :deletedDate where id = :id")
-    void delete(@Param("id") Long id, LocalDate deletedDate);
+    @Query("update Customer set deletedAt = current_timestamp where id = :id")
+    void delete(@Param("id") Long id);
 
     @Modifying
     @Query("update Customer set active = :isActive where id = :id")
