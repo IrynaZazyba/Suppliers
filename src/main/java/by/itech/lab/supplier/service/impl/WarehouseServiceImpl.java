d120c2ebc61c00790129054ca4c9eca0674f0a69
alexeyoverchenko <alexeyoverchenko@gmail.com>
2020-11-05 00:57:50 +0300

    - in progress

--- src/main/java/by/itech/lab/supplier/service/impl/WarehouseServiceImpl.java
+++ src/main/java/by/itech/lab/supplier/service/impl/WarehouseServiceImpl.java
@@ -1,12 +1,31 @@
 package by.itech.lab.supplier.service.impl;
 
+import by.itech.lab.supplier.domain.Warehouse;
+import by.itech.lab.supplier.dto.WarehouseDto;
+import by.itech.lab.supplier.dto.mapper.WarehouseMapper;
+import by.itech.lab.supplier.exception.NotFoundInDBException;
+import by.itech.lab.supplier.repository.WarehouseRepository;
 import by.itech.lab.supplier.service.WarehouseService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class WarehouseServiceImpl implements WarehouseService {
+
+    private final WarehouseRepository warehouseRepository;
+    private final WarehouseMapper warehouseMapper;
+
+    @Override
+    public Page<WarehouseDto> findAllByActive(Pageable pageable, Boolean active) {
+        return null;
+    }
 
     @Override
     public WarehouseDto findById(Long warehouseId) {
@@ -36,5 +55,4 @@ public class WarehouseServiceImpl implements WarehouseService {
     public void delete(final Long id) {
         warehouseRepository.delete(id, LocalDate.now());
     }
-
 }
