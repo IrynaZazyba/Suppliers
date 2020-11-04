b428631a7c142777a30414e4e209f41c853ba723
IrynaZazyba <38259449+IrynaZazyba@users.noreply.github.com>
2020-11-04 16:51:43 +0300

    customer page

--- frontend/src/pages/Customers.js
+++ frontend/src/pages/Customers.js
@@ -1,7 +1,10 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext} from 'react';
 import Button from "react-bootstrap/Button";
 import {AuthContext} from "../context/authContext";
 import Container from "react-bootstrap/Container";
+import Table from "react-bootstrap/Table";
+import Form from 'react-bootstrap/Form'
+import {FaEdit} from "react-icons/fa";
 
 export default () => {
     const {user, setUser} = useContext(AuthContext);
@@ -19,12 +22,52 @@ export default () => {
 
     return (
         <Container fluid className="mainContainer">
-          <Button size="md"
-                  type="submit"
-                  onClick={handleSubmit}
-                  className="mainButton">
-              Add
-          </Button>
-        </Container>    );
+            <Button size="md"
+                    type="button"
+                    onClick={handleSubmit}
+                    className="mainButton">
+                Add
+            </Button>
+            <Button size="md"
+                    type="button"
+                    onClick={handleSubmit}
+                    className="deleteButton">
+                Remove
+            </Button>
+            <Table  bordered hover size="sm" style={{marginTop:'25px'}}>
+                <thead>
+                <tr>
+                    <th style={{width: '2%'}}></th>
+                    <th style={{width: '13%'}}>Name</th>
+                    <th style={{width: '11%'}}>Registration date</th>
+                    <th style={{width: '3%'}}>status</th>
+                    <th style={{width: '15%'}}>email of admin</th>
+                    <th style={{width: '2%'}}></th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td><Form.Check type="checkbox"/></td>
+                    <td>Mark</td>
+                    <td>04.11.2020</td>
+                    <td><Form.Check
+                        type="switch"
+                        id="custom-switch"
+                   style={{width:'25px'}}/>
+                    </td>
+                    <td>admin@mdo</td>
+                    <td><Button variant="link"><FaEdit style={{textAlign:'center', color: '#1A7FA8'}} size={'1.3em'}/></Button></td>
+                </tr>
+                <tr>
+                    <td><Form.Check type="checkbox"/></td>
+                    <td>Jacob</td>
+                    <td>04.11.2020</td>
+                    <td>@fat</td>
+                    <td>admin@mdo</td>
+                    <td><Button variant="link"><FaEdit style={{textAlign:'center',  color: '#1A7FA8'}} size={'1.3em'}/></Button></td>
+                </tr>
+                </tbody>
+            </Table>
+        </Container>);
 
 }
