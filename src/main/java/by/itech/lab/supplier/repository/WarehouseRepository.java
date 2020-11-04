f439fe06d073dcddcb0f880a1bb3c1d883256e6c
alexeyoverchenko <alexeyoverchenko@gmail.com>
2020-11-04 18:52:42 +0300

    - finished Warehouse get info methods

--- src/main/java/by/itech/lab/supplier/repository/WarehouseRepository.java
+++ src/main/java/by/itech/lab/supplier/repository/WarehouseRepository.java
@@ -1,13 +1,10 @@
 package by.itech.lab.supplier.repository;
 
 import by.itech.lab.supplier.domain.Warehouse;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
 
-    Page<Warehouse> findAll(Pageable pageable);
 }
