6965e8b1117ce00d9118812e2f4e67bbd08ace61
IrynaZazyba <38259449+IrynaZazyba@users.noreply.github.com>
2020-11-11 10:27:36 +0300

    added validation to dto

--- src/main/java/by/itech/lab/supplier/dto/AddressDto.java
+++ src/main/java/by/itech/lab/supplier/dto/AddressDto.java
@@ -3,13 +3,22 @@ package by.itech.lab.supplier.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Data
 @Builder
 public class AddressDto implements BaseDto {
 
     private Long id;
+    @NotBlank
+    @Size(min = 1, max = 50)
     private String city;
+    @NotBlank
+    @Size(min = 1, max = 50)
     private String addressLine1;
+    @Size(min = 1, max = 50)
+    @NotBlank
     private String addressLine2;
     private StateDto state;
 
--- src/main/java/by/itech/lab/supplier/dto/StateDto.java
+++ src/main/java/by/itech/lab/supplier/dto/StateDto.java
@@ -3,11 +3,16 @@ package by.itech.lab.supplier.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Data
 @Builder
 public class StateDto implements BaseDto {
 
     private Long id;
+    @NotBlank
+    @Size(min = 1, max = 50)
     private String state;
 
 }
