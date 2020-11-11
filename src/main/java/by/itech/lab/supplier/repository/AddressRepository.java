706f357b7608d8da28328c5bd51318ea6a35b80f
IrynaZazyba <38259449+IrynaZazyba@users.noreply.github.com>
2020-11-11 11:52:38 +0300

    short fix

--- src/main/java/by/itech/lab/supplier/repository/AddressRepository.java
+++
@@ -1,8 +0,0 @@
-package by.itech.lab.supplier.repository;
-
-import by.itech.lab.supplier.domain.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-
-}
