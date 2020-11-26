7cba5c5d95099bce291a03e3bdfebe9e37c8a65c
IrynaZazyba <38259449+IrynaZazyba@users.noreply.github.com>
2020-11-27 00:26:29 +0300

    short fix

--- src/main/java/by/itech/lab/supplier/controller/ApplicationController.java
+++ src/main/java/by/itech/lab/supplier/controller/ApplicationController.java
@@ -37,21 +37,21 @@ public class ApplicationController {
     private final ApplicationService applicationService;
 
     @PostMapping
-//    @Secured({"ROLE_DISPATCHER", "ROLE_LOGISTICS_SPECIALIST", "ROLE_SYSTEM_ADMIN"})
+    @Secured({"ROLE_DISPATCHER", "ROLE_LOGISTICS_SPECIALIST", "ROLE_SYSTEM_ADMIN"})
     public ApplicationDto save(@Valid @RequestBody ApplicationDto applicationDto) {
         return applicationService.save(applicationDto);
     }
 
     @PutMapping(ApiConstants.URL_ID_PARAMETER + ApiConstants.URL_STATUS_PARAMETER)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-//    @Secured({"ROLE_DISPATCHER", "ROLE_LOGISTICS_SPECIALIST", "ROLE_SYSTEM_ADMIN"})
+    @Secured({"ROLE_DISPATCHER", "ROLE_LOGISTICS_SPECIALIST", "ROLE_SYSTEM_ADMIN"})
     public void changeStatus(@PathVariable Long id,
                              @PathVariable String status) {
         applicationService.changeStatus(id, ApplicationStatus.valueOf(status));
     }
 
     @GetMapping
-//    @Secured({"ROLE_DISPATCHER", "ROLE_LOGISTICS_SPECIALIST"})
+    @Secured({"ROLE_DISPATCHER", "ROLE_LOGISTICS_SPECIALIST"})
     public Page<ApplicationDto> getAllByStatus(@PageableDefault final Pageable pageable,
                                                @RequestParam(required = false) final ApplicationStatus status) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -64,27 +64,27 @@ public class ApplicationController {
     }
 
     @GetMapping(ApiConstants.URL_ADMIN)
-//    @Secured("ROLE_SYSTEM_ADMIN")
+    @Secured("ROLE_SYSTEM_ADMIN")
     public Page<ApplicationDto> getAllAdminByStatus(@PageableDefault final Pageable pageable,
                                                     @RequestParam(required = false) final ApplicationStatus status) {
         return applicationService.findAllByStatus(pageable, status);
     }
 
     @GetMapping(ApiConstants.URL_ID_PARAMETER)
-//    @Secured({"ROLE_DISPATCHER", "ROLE_LOGISTICS_SPECIALIST", "ROLE_SYSTEM_ADMIN"})
+    @Secured({"ROLE_DISPATCHER", "ROLE_LOGISTICS_SPECIALIST", "ROLE_SYSTEM_ADMIN"})
     public ApplicationDto getById(@PathVariable Long id) {
         return applicationService.findById(id);
     }
 
     @GetMapping(ApiConstants.URL_NUMBER + ApiConstants.URL_NUMBER_PARAMETER)
-//    @Secured({"ROLE_DISPATCHER", "ROLE_LOGISTICS_SPECIALIST", "ROLE_SYSTEM_ADMIN"})
+    @Secured({"ROLE_DISPATCHER", "ROLE_LOGISTICS_SPECIALIST", "ROLE_SYSTEM_ADMIN"})
     public ApplicationDto getByNumber(@PathVariable String number) {
         return applicationService.findByNumber(number);
     }
 
     @DeleteMapping(ApiConstants.URL_ID_PARAMETER)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-//    @Secured({"ROLE_DISPATCHER", "ROLE_LOGISTICS_SPECIALIST", "ROLE_SYSTEM_ADMIN"})
+    @Secured({"ROLE_DISPATCHER", "ROLE_LOGISTICS_SPECIALIST", "ROLE_SYSTEM_ADMIN"})
     public void delete(@PathVariable Long id) {
         applicationService.delete(id);
     }
