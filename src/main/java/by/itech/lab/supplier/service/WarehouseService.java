ccbd057b9bbb20afd045c6c6ac9a11755ee83f3e
alexeyoverchenko <alexeyoverchenko@gmail.com>
2020-12-08 14:21:41 +0300

    - deleted unused import

--- src/main/java/by/itech/lab/supplier/service/WarehouseService.java
+++ src/main/java/by/itech/lab/supplier/service/WarehouseService.java
@@ -9,7 +9,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 public interface WarehouseService{
