b8758c1e6387c26c02f3c238b16b435684e817da
IrynaZazyba <38259449+IrynaZazyba@users.noreply.github.com>
2020-12-10 11:37:02 +0300

    added opportunity move by route according to waypoints order, added showing the route on the map

--- frontend/src/pages/waybill/DeliveryModal.js
+++ frontend/src/pages/waybill/DeliveryModal.js
@@ -1,3 +1,5 @@
+/*global google*/
+/* eslint-disable no-undef */
 import ErrorMessage from "../../messages/errorMessage";
 import {Row} from "react-bootstrap";
 import Col from "react-bootstrap/Col";
@@ -6,6 +8,10 @@ import Card from "react-bootstrap/Card";
 import Modal from "react-bootstrap/Modal";
 import {AuthContext} from "../../context/authContext";
 import Badge from "react-bootstrap/Badge";
+import MyMapComponent from "../../components/MyMapComponent";
+import ListGroup from "react-bootstrap/ListGroup";
+import {FaCheck, FaFlag, FaRegSquare} from "react-icons/fa";
+import Button from "react-bootstrap/Button";
 
 function DeliveryModal(props) {
 
@@ -16,16 +22,109 @@ function DeliveryModal(props) {
     const {user, setUser} = useContext(AuthContext);
     const customerId = user.currentCustomerId;
     const [waybill, setWaybill] = useState();
+    const [mapCenter, setMapCenter] = useState({lat: 51.494900, lng: -0.146231});
+    const [directions, setDirections] = useState();
+    const [waypoints, setWaypoints] = useState([]);
+    const [startPoint, setStartPoint] = useState();
+    const [blockedPoints, setBlockedPoints] = useState([]);
 
     useEffect(() => {
         if (props.modal.isOpen === true) {
             fetch(`/customers/${customerId}/waybills/${props.modal.waybillId}`)
                 .then(response => response.json())
                 .then(res => {
+                    let blockedPointId = res.route.wayPoints.filter(point => point.priority !== 1).map(p => p.id);
+                    setBlockedPoints(blockedPointId);
                     setWaybill(res);
+                    let route = res.route;
+                    let countPoints = route.wayPoints.length;
+                    let points = route.wayPoints.filter(wp => (wp.priority !== 0 && wp.priority !== (countPoints - 1)));
+                    let endPoint = route.wayPoints.filter(wp => wp.priority === (countPoints - 1));
+                    let startPoint = route.wayPoints.filter(wp => wp.priority === 0);
+                    setMapCenter({lat: startPoint[0].address.latitude, lng: startPoint[0].address.longitude});
+                    let waypoints = points.map(wp => {
+                        return {
+                            location: {
+                                lat: wp.address.latitude,
+                                lng: wp.address.longitude
+                            },
+                            stopover: true
+                        }
+                    });
+                    let waypointsWithEnd = [...points, ...endPoint];
+                    setWaypoints(waypointsWithEnd);
+                    setStartPoint(startPoint[0]);
+                    let start = {lat: startPoint[0].address.latitude, lng: startPoint[0].address.longitude};
+                    let end = {
+                        lat: endPoint[0].address.latitude,
+                        lng: endPoint[0].address.longitude
+                    };
+                    renderRoute(start, end, waypoints);
                 });
         }
     }, [props.modal]);
+
+    const setVisited = (e) => {
+        e.preventDefault();
+        let wayPointId = parseInt(e.target.id);
+        if (!blockedPoints.includes(wayPointId)) {
+            let points = JSON.parse(JSON.stringify(waypoints));
+            points.forEach(wp => {
+                    if (wp.id === wayPointId) {
+                        wp.visited = true;
+                    }
+                }
+            );
+            setWaypoints(points);
+            let blocked = blockedPoints.slice();
+            blocked.push(wayPointId);
+            let afterRemove = blocked.filter(id => id !== (wayPointId + 1));
+            setBlockedPoints(afterRemove);
+        }
+    };
+
+
+    function renderRoute(start, end, waypoints) {
+        const directionsService = new google.maps.DirectionsService();
+        let directionsRenderer = new google.maps.DirectionsRenderer();
+
+        let requests = {
+            origin: start,
+            destination: end,
+            waypoints: waypoints,
+            travelMode: 'DRIVING'
+        };
+        directionsService.route(requests, function (result, status) {
+            if (status == 'OK') {
+                setDirections(result);
+                directionsRenderer.setDirections(result);
+            } else {
+                setErrors(prevState => ({
+                    ...prevState,
+                    serverErrors: "Something went wrong, please try later",
+                }));
+            }
+        });
+    }
+
+
+    const isVisited = (wp) => {
+        return wp.visited ? <FaCheck id={wp.id}
+                                     style={{marginRight: '5px', textAlign: 'center', color: 'green'}}
+                                     size={'1.5em'}/> :
+            <FaRegSquare id={wp.id}
+                         style={{
+                             marginRight: '5px',
+                             textAlign: 'center',
+                             color: '#1A7FA8'
+                         }}
+                         size={'1.4em'}
+                         onClick={setVisited}/>;
+    };
+
+    const startDelivery = <Button type="submit" className="delivery-button"
+        //onClick={startDelivery}
+    >Start delivery</Button>;
 
     const waybillInfo =
         <>
@@ -69,18 +168,35 @@ function DeliveryModal(props) {
                     </Row>
                 </Col>
                 <Col sm={4}>
-
+                    {startDelivery}
                 </Col>
             </Row>
             }
         </>;
 
+    const map =
+        <MyMapComponent
+            mapCenter={mapCenter}
+            directions={directions}
+            isMarkerShown
+            googleMapURL="https://maps.googleapis.com/maps/api/js?key=API_KEY&callback=initMap&libraries=geometry,drawing,places"
+            loadingElement={<div style={{height: `100%`}}/>}
+            containerElement={<div style={{height: `350px`}}/>}
+            mapElement={<div style={{height: `100%`}}/>}
+
+        />;
 
     return (
         <>
             <Modal
                 show={props.modal.isOpen}
                 onHide={() => {
+                    setBlockedPoints([]);
+                    setWaypoints([]);
+                    setWaybill();
+                    setDirections([]);
+                    setStartPoint();
+                    setErrors([]);
                     props.onChange(false)
                 }}
                 aria-labelledby="modal-custom"
@@ -112,8 +228,24 @@ function DeliveryModal(props) {
 
                     <Row style={{marginTop: '15px'}}>
                         <Col sm={6}>
+                            {map}
                         </Col>
-                        <Col sm={6}>
+                        <Col id="google-panel" sm={6}>
+                            <ListGroup variant="flush">
+                                {startPoint && <ListGroup.Item key={startPoint.id}>
+                                    <FaFlag id={startPoint.id}
+                                            style={{marginRight: '5px', textAlign: 'center', color: 'green'}}
+                                            size={'1.5em'}/>
+                                    {startPoint.address.state.state}, {startPoint.address.city}, {startPoint.address.addressLine1}, {startPoint.address.addressLine2}
+                                </ListGroup.Item>}
+                                {waypoints.sort((a, b) => a.priority - b.priority).map(wp => (
+                                    <ListGroup.Item key={wp.id}
+                                                    className={blockedPoints.includes(wp.id) ? 'delivery-blocked-waypoint' : ''}>
+                                        {isVisited(wp)}
+                                        {wp.address.state.state}, {wp.address.city}, {wp.address.addressLine1}, {wp.address.addressLine2}
+                                    </ListGroup.Item>
+                                ))}
+                            </ListGroup>
                         </Col>
                     </Row>
                     <div className="float-right" style={{padding: '10px'}}>
